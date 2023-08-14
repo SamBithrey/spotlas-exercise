7812dcbb70ee42d5e939309e175fc3cf32ed6fe7
SamBithrey <sambithrey@gmail.com>
2023-08-14 17:32:31 +0100

    endpoint complete for circles

--- go.mod
+++ go.mod
@@ -2,9 +2,18 @@ module github.com/SamBithrey/spotlas-exercise
 
 go 1.21.0
 
-require github.com/gofiber/fiber/v2 v2.48.0
+require (
+	github.com/gofiber/fiber/v2 v2.48.0
+	github.com/lib/pq v1.10.9
+)
 
 require (
+	github.com/lann/builder v0.0.0-20180802200727-47ae307949d0 // indirect
+	github.com/lann/ps v0.0.0-20150810152359-62de8c46ede0 // indirect
+)
+
+require (
+	github.com/Masterminds/squirrel v1.5.4
 	github.com/andybalholm/brotli v1.0.5 // indirect
 	github.com/google/uuid v1.3.0 // indirect
 	github.com/klauspost/compress v1.16.3 // indirect
--- main.go
+++ main.go
@@ -1,19 +1,124 @@
 package main
 
 import (
+	"database/sql"
 	"fmt"
 	"log"
 
+	sq "github.com/Masterminds/squirrel"
 	"github.com/gofiber/fiber/v2"
+	_ "github.com/lib/pq"
 )
 
 func main() {
+	psql := sq.StatementBuilder.PlaceholderFormat(sq.Dollar)
 	app := fiber.New()
 
 	fmt.Println("Server running OK!")
 
 	app.Get("/healthcheck", func(c *fiber.Ctx) error {
 		return c.SendString("OK")
+	})
+
+	connectionStr := "user=postgres dbname=spotlas-work port=5432 sslmode=disable"
+
+	app.Get("/allspots", func(c *fiber.Ctx) error {
+		conn, err := sql.Open("postgres", connectionStr)
+		if err != nil {
+			panic(err)
+		}
+
+		sql, _, _ := psql.Select("name").From("\"MY_TABLE\"").ToSql()
+		allspots, err := conn.Query(sql)
+		if err != nil {
+			panic(err)
+		}
+
+		names := make([]string, 0)
+		for allspots.Next() {
+			var name string
+			if err := allspots.Scan(&name); err != nil {
+				log.Fatal(err)
+			}
+			names = append(names, name)
+		}
+
+		conn.Close()
+
+		return c.JSON(names)
+	})
+
+	type DistanceParams struct {
+		Lat    float64 `query:"lat"`
+		Lon    float64 `query:"lon"`
+		Radius int     `query:"radius"`
+		Shape  string  `query:"shape"`
+	}
+
+	type Spot struct {
+		Id          string  `json:"id"`
+		Name        string  `json:"name"`
+		Website     any     `json:"website"`
+		Coordinates string  `json:"coordinates"`
+		Description any     `json:"description"`
+		Rating      float64 `json:"rating"`
+	}
+
+	app.Get("/distance", func(c *fiber.Ctx) error {
+		p := new(DistanceParams)
+		p.Shape = "circle"
+
+		if err := c.QueryParser(p); err != nil {
+			return err
+		}
+
+		if p.Lat == 0 || p.Lon == 0 {
+			return c.Status(404).JSON(`Error:Please input coordinates`)
+		}
+		if p.Radius == 0 {
+			return c.Status(404).JSON(`Error:Please input radius`)
+		}
+
+		conn, err := sql.Open("postgres", connectionStr)
+		if err != nil {
+			panic(err)
+		}
+
+		lat := fmt.Sprintf("%v", p.Lat)
+		lon := fmt.Sprintf("%v", p.Lon)
+		spots := []Spot{}
+
+		if p.Shape == "circle" {
+			filter := fmt.Sprintf("ST_DWithin(coordinates, 'SRID=4326;POINT(%v %v)'::geography, %v)", lon, lat, p.Radius)
+			order := fmt.Sprintf("ST_Distance(coordinates, 'SRID=4326;POINT(%v %v)'::geography)", lon, lat)
+
+			sql, _, _ := psql.Select("*").From("\"MY_TABLE\"").Where(filter).OrderBy(order).ToSql()
+			var spot Spot
+			rows, err := conn.Query(sql)
+			if err != nil {
+				panic(err)
+			}
+
+			for rows.Next() {
+				spot.Id = ""
+				spot.Name = ""
+				spot.Website = ""
+				spot.Coordinates = ""
+				spot.Description = ""
+				spot.Rating = 0
+				if err := rows.Scan(&spot.Id, &spot.Name, &spot.Website, &spot.Coordinates, &spot.Description, &spot.Rating); err != nil {
+					log.Fatal(err)
+				}
+				spots = append(spots, spot)
+			}
+
+		} else if p.Shape == "square" {
+
+		} else {
+			return c.Status(404).JSON(`Error:Please input correct shape`)
+		}
+
+		return c.JSON(spots)
 	})
 
 	log.Fatal(app.Listen(":4000"))
