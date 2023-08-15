5ca1dffe7b9ec8b95a4cbb41aed7f31c87aa66e3
SamBithrey <sambithrey@gmail.com>
2023-08-15 11:18:14 +0100

    Clean up/Comments/Postman Testing collection

--- main.go
+++ main.go
@@ -22,32 +22,6 @@ func main() {
 
 	connectionStr := "user=postgres dbname=spotlas-work port=5432 sslmode=disable"
 
-	app.Get("/allspots", func(c *fiber.Ctx) error {
-		conn, err := sql.Open("postgres", connectionStr)
-		if err != nil {
-			panic(err)
-		}
-
-		sql, _, _ := psql.Select("name").From("\"MY_TABLE\"").ToSql()
-		allspots, err := conn.Query(sql)
-		if err != nil {
-			panic(err)
-		}
-
-		names := make([]string, 0)
-		for allspots.Next() {
-			var name string
-			if err := allspots.Scan(&name); err != nil {
-				log.Fatal(err)
-			}
-			names = append(names, name)
-		}
-
-		conn.Close()
-
-		return c.JSON(names)
-	})
-
 	type DistanceParams struct {
 		Lat    float64 `query:"lat"`
 		Lon    float64 `query:"lon"`
@@ -58,88 +32,92 @@ func main() {
 	type Spot struct {
 		Id          string  `json:"id"`
 		Name        string  `json:"name"`
-		Website     any     `json:"website"`
+		Website     any     `json:"website"` // typed as any to allow for null values
 		Coordinates string  `json:"coordinates"`
-		Description any     `json:"description"`
+		Description any     `json:"description"` // typed as any to allow for null values
 		Rating      float64 `json:"rating"`
 	}
 
 	app.Get("/distance", func(c *fiber.Ctx) error {
 		p := new(DistanceParams)
+		// shape param defaults to circle if no value entered.
 		p.Shape = "circle"
 
+		// Get the query params and type check using DistanceParams struct typing.
 		if err := c.QueryParser(p); err != nil {
-			return err
+			return c.Status(400).JSON(`Error:Incorrect query params`)
 		}
 
+		// if no values for latitude and longitude are entered return error.
 		if p.Lat == 0 || p.Lon == 0 {
-			return c.Status(404).JSON(`Error:Please input coordinates`)
+			return c.Status(400).JSON(`Error:Please input coordinates`)
 		}
+		// if no value for Radius is entered return error.
 		if p.Radius == 0 {
-			return c.Status(404).JSON(`Error:Please input radius`)
+			return c.Status(400).JSON(`Error:Please input radius`)
 		}
 
+		// Connect to DB
 		conn, err := sql.Open("postgres", connectionStr)
 		if err != nil {
 			panic(err)
+		} else {
+			fmt.Println("Connection to db made.")
 		}
 
-		lat := fmt.Sprintf("%v", p.Lat)
-		lon := fmt.Sprintf("%v", p.Lon)
+		// Create spots slice.
 		spots := []Spot{}
+		// Create spot object.
+		var spot Spot
 
 		if p.Shape == "circle" {
-			filter := fmt.Sprintf("ST_DWithin(coordinates, 'SRID=4326;POINT(%v %v)'::geography, %v)", lon, lat, p.Radius)
-			order := fmt.Sprintf("ST_Distance(coordinates, 'SRID=4326;POINT(%v %v)'::geography)", lon, lat)
-
+			// Create Filter string
+			filter := fmt.Sprintf("ST_DWithin(coordinates, 'SRID=4326;POINT(%v %v)'::geography, %v)", p.Lon, p.Lat, p.Radius)
+			// Create Order string
+			order := fmt.Sprintf("ST_Distance(coordinates, 'SRID=4326;POINT(%v %v)'::geography)", p.Lon, p.Lat)
+			// Create SQL string
 			sql, _, _ := psql.Select("*").From("\"MY_TABLE\"").Where(filter).OrderBy(order).ToSql()
-			var spot Spot
+			// Query DB
 			rows, err := conn.Query(sql)
 			if err != nil {
 				panic(err)
 			}
-
+			// Iterate through rows and enter data into the spot object
 			for rows.Next() {
-				spot.Id = ""
-				spot.Name = ""
-				spot.Website = ""
-				spot.Coordinates = ""
-				spot.Description = ""
-				spot.Rating = 0
 				if err := rows.Scan(&spot.Id, &spot.Name, &spot.Website, &spot.Coordinates, &spot.Description, &spot.Rating); err != nil {
 					log.Fatal(err)
 				}
+				// Append spot object to spots slice
 				spots = append(spots, spot)
 			}
 
 		} else if p.Shape == "square" {
-			filter := fmt.Sprintf("coordinates && ST_Buffer('SRID=4326;POINT(%v %v)'::geography, %v, 'quad_segs=1')", lon, lat, p.Radius)
-			order := fmt.Sprintf("ST_Distance(coordinates, 'SRID=4326;POINT(%v %v)'::geography)", lon, lat)
-
+			// Create Filter string
+			filter := fmt.Sprintf("coordinates && ST_Buffer('SRID=4326;POINT(%v %v)'::geography, %v, 'quad_segs=1')", p.Lon, p.Lat, p.Radius)
+			// Create Order string
+			order := fmt.Sprintf("ST_Distance(coordinates, 'SRID=4326;POINT(%v %v)'::geography)", p.Lon, p.Lat)
+			// Create SQL string
 			sql, _, _ := psql.Select("*").From("\"MY_TABLE\"").Where(filter).OrderBy(order).ToSql()
-			var spot Spot
+			// Query DB
 			rows, err := conn.Query(sql)
 			if err != nil {
 				panic(err)
 			}
-
+			// Iterate through rows and enter data into the spot object
 			for rows.Next() {
-				spot.Id = ""
-				spot.Name = ""
-				spot.Website = ""
-				spot.Coordinates = ""
-				spot.Description = ""
-				spot.Rating = 0
 				if err := rows.Scan(&spot.Id, &spot.Name, &spot.Website, &spot.Coordinates, &spot.Description, &spot.Rating); err != nil {
 					log.Fatal(err)
 				}
+				// Append spot object to spots slice
 				spots = append(spots, spot)
 			}
 
 		} else {
-			return c.Status(404).JSON(`Error:Please input correct shape`)
+			// If shape is any other value than "square" or "circle" return error.
+			return c.Status(400).JSON(`Error:Please input correct shape`)
 		}
-
+		defer conn.Close()
+		defer fmt.Println("Connection to db closed")
 		return c.JSON(spots)
 	})
 
